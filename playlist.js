365d9caedb7cad85b13d4c47cebce8803e7615af
KiranKhanna721 <76143695+KiranKhanna721@users.noreply.github.com>
2021-10-24 18:27:11 +0530

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -3,28 +3,28 @@ trackList = [
     name:"On My Way",
       singer: "Alan Walker",
       artist:"Alan Walker",
-    src:"music/On_My_Way.mp3"
+    src:"media/room2/music/On_My_Way.mp3"
   },
           
   {
     name: "Alone Part II",
     artist:"Alan Walker",
-    src: "music/Alone_Part_II.mp3"
+    src: "media/room2/music/Alone_Part_II.mp3"
   },
           
   {
     name: "Faded",
     artist:"Alan Walker",
-    src: "music/Faded.mp3"
+    src: "media/room2/music/Faded.mp3"
   },
   {
     name: "You are not alone",
     artist:"Michael Jackson",
-    src: "music/You_Are_Not_Alone.mp3"
+    src: "media/room2/music/You_Are_Not_Alone.mp3"
   },
   {
     name: "Legends Never Die",
     artist:"Alan Walker",
-    src: "music/Legends_Never_Die.mp3"
+    src: "media/room2/music/Legends_Never_Die.mp3"
   }
 ];
