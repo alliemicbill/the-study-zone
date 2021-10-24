d108cdb61c718e7c8dea8aecc003ab8d9ecbb819
KiranKhanna721 <76143695+KiranKhanna721@users.noreply.github.com>
2021-10-24 17:36:45 +0530

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,30 @@
+trackList = [
+  {
+    name:"On My Way",
+      singer: "Alan Walker",
+      artist:"Alan Walker",
+    src:"music/On_My_Way.mp3"
+  },
+          
+  {
+    name: "Alone Part II",
+    artist:"Alan Walker",
+    src: "music/Alone_Part_II.mp3"
+  },
+          
+  {
+    name: "Faded",
+    artist:"Alan Walker",
+    src: "music/Faded.mp3"
+  },
+  {
+    name: "You are not alone",
+    artist:"Michael Jackson",
+    src: "music/You_Are_Not_Alone.mp3"
+  },
+  {
+    name: "Legends Never Die",
+    artist:"Alan Walker",
+    src: "music/Legends_Never_Die.mp3"
+  }
+];
